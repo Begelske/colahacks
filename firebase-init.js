39d74eaa2619f8b9f260504e78ed35a008d4f5d8
Melissa Thompson <mthompson828@gmail.com>
2018-04-14 09:46:43 -0400

    firebase init

---
+++ firebase-init.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDvl8v-HMrkw7GRI76vNvHAOPhf5kGeWtg",
+  authDomain: "colahacks-cdccd.firebaseapp.com",
+  databaseURL: "https://colahacks-cdccd.firebaseio.com",
+  projectId: "colahacks-cdccd",
+  storageBucket: "",
+  messagingSenderId: "121062411057"
+};
+firebase.initializeApp(config);
